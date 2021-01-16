7ba4fd8929b739f9c1f54576a47ac082bd12778f
Mohammad Abdur Rahman <rahman.just16@gmail.com>
2021-01-16 22:56:08 +0600

    array

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-// var date= new Date;
-// console.log("Todays Work"+date);
+// // var date= new Date;
+// // console.log("Todays Work"+date);
 
-var friendsAge= [15,17,18,20];
-var friendsName = ['samia', 'lamia','chadny'];
-friendsAge[0]= 16;
-var position= friendsAge.indexOf(17);
-console.log(friendsAge[0]);
-console.log(position);
-friendsAge.push(0); //last a add hoiche
-console.log(friendsAge.length);
-friendsName.pop();
-console.log(friendsName);
-//add elements begining or remove
+// var friendsAge= [15,17,18,20];
+var friendsName = ['samia', 'lamia','chadny','adiba','sania'];
+// friendsAge[0]= 16;
+// var position= friendsAge.indexOf(17);
+// console.log(friendsAge[0]);
+// console.log(position);
+// friendsAge.push(0); //last a add hoiche
+// console.log(friendsAge.length);
+// friendsName.pop();
+// console.log(friendsName);
+// //add elements begining or remove
 
-friendsName.unshift('babu');
-console.log(friendsName);
+// friendsName.unshift('babu');
+// console.log(friendsName);
 
-friendsName.shift('babu');
-console.log(friendsName);
+// friendsName.shift('babu');
+// console.log(friendsName);
+ var part = friendsName.slice(2, 4)
+ console.log(part);
+ console.log(friendsName);
