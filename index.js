112558749851866dd35709b22ad7ef9070515d27
Mohammad Abdur Rahman <rahman.just16@gmail.com>
2021-01-16 22:36:08 +0600

    array

---
+++ index.js
@@ -0,0 +1,10 @@
+// var date= new Date;
+// console.log("Todays Work"+date);
+
+var friendsAge= [15,17,18,20];
+var friendsName = ['samia', 'lamia','chadny'];
+friendsAge[0]= 16;
+var position= friendsAge.indexOf(17);
+
+console.log(friendsAge[0]);
+console.log(position);
